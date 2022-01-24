c3829226235c3347602b57b200b64669f65d54ff
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-01-24 11:45:36 +0100

    Added Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour{
+
+    Rigidbody _Rigidbody;
+    
+    public float moveSpeed = 5;
+
+    void Awake(){
+        _Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        
+        //Get move input
+        //Pref get input in Update()
+        var moveInput = Input.GetAxis("Horizontal");
+        
+        //Set move velocity
+        //Pref interact with physics in FixedUpdate()
+        _Rigidbody.velocity = new Vector3(moveSpeed * moveInput, _Rigidbody.velocity.y, 0);
+        
         
     }
 }
