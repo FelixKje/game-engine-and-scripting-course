470968534e0e070a46b672e6578c56cced6c78cb
Felix K <felix.kjellberg@student.forsbergsskola.se>
2022-01-24 11:58:47 +0100

    Added Jumping to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,9 @@ public class PlayerController : MonoBehaviour{
 
     Rigidbody _Rigidbody;
     
-    public float moveSpeed = 5;
+    public float moveSpeed = 5f;
+
+    public float jumpForce = 500f;
 
     void Awake(){
         _Rigidbody = GetComponent<Rigidbody>();
@@ -20,7 +22,14 @@ public class PlayerController : MonoBehaviour{
         //Set move velocity
         //Pref interact with physics in FixedUpdate()
         _Rigidbody.velocity = new Vector3(moveSpeed * moveInput, _Rigidbody.velocity.y, 0);
+
+        //Get move input
+        //Pref get input in Update()
+        var jumpInput = Input.GetKeyDown(KeyCode.Space);
         
-        
+        //Apply jump force
+        //Pref interact with physics in FixedUpdate()
+        if (jumpInput && _Rigidbody.velocity.y == 0)
+            _Rigidbody.AddForce(Vector3.up * jumpForce);
     }
 }
